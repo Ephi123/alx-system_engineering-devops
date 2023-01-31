92aa96ab31942966745799ef58f863271326e0e3
Ephi123 <ephrembereket36@gmail.com>
2023-01-31 13:03:08 -0800

    regx

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,3 @@
-#!/usr/bin/env ruby
-# Simply matching School
-
+#!/usr/bin/env ruby                                                                                               
+#simple matching Scholl
+School
