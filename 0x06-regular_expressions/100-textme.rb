363e5899f28327a9b7ce4eb435c437c1c96996a5
Ephi123 <99554853+Ephi123@users.noreply.github.com>
2023-02-01 01:07:15 +0300

    regx

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:([a-zA-Z0-9+-]+)]\s\[to:([a-zA-Z0-9+-]+)]\s\[flags:([0-9-:]+)]/).join
-$1$2$3
+puts ARGV[0].scan(/\[from:([a-zA-Z0-9+-]+)]\s\[to:([a-zA-Z0-9+-]+)]\s\[flags:([0-9-:]+)]/).join(",")
